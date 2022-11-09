cd801df9beae986795728434119e57974c58fd2b
AyseCKR69 <ayseckr699@gmail.com>
2022-11-09 23:45:16 +0300

    23:45

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { ProductComponent } from './components/product/product.component';
+
 
 @NgModule({
   declarations: [
@@ -13,10 +16,14 @@ import { NaviComponent } from './components/navi/navi.component';
     ProductComponent,
     CategoryComponent,
     NaviComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgFor,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
-
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  todos: Todo[] = [];
+  dataLoaded=false;
 
-todos:Todo[]=[];
-
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.getTodos();
   }
 
+  // asenkron olduğu için subscrible yazıyoruz
+  getTodos() {
+    this.todoService.getTodos().subscribe((response) => {
+      this.todos = response;
+      this.dataLoaded=true;
+    });
+  }
 }
