90e4824b9166e8ca6d32ce6cd93400e31b715b81
AyseCKR69 <ayseckr699@gmail.com>
2022-11-15 17:42:40 +0300

    17:42

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import{FormsModule} from "@angular/forms"
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { CommonModule, NgFor } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
 import { AttAddedPipe } from './pipes/att-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+import { ToastrModule } from 'ngx-toastr';
+
 
 
 @NgModule({
@@ -27,9 +30,14 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgFor,
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Todo } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
@@ -10,43 +11,45 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class ProductComponent implements OnInit {
   todos: Todo[] = [];
-  dataLoaded=false;
-  filterText="";
-//ActivatedRoute=aktifleştirilmiş ruut demek yani url okuma
-  constructor(private todoService: TodoService,
-    private activatedRoute:ActivatedRoute) {}
+  dataLoaded = false;
+  filterText = '';
+  //ActivatedRoute=aktifleştirilmiş ruut demek yani url okuma
+  constructor(
+    private todoService: TodoService,
+
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
-
     this.getTodos();
-    
-  //    this.activatedRoute.params.subscribe(params=>{
-  //    if(params["categoryId"]){
-  //     this.getTodosByCategory(params["categoryId"])
-  //     }else{
-  //        this.getTodos()
-  //     }
-  //  })
+
+    //    this.activatedRoute.params.subscribe(params=>{
+    //    if(params["categoryId"]){
+    //     this.getTodosByCategory(params["categoryId"])
+    //     }else{
+    //        this.getTodos()
+    //     }
+    //  })
   }
 
   // asenkron olduğu için subscrible yazıyoruz
   getTodos() {
     this.todoService.getTodos().subscribe((response) => {
-     
       this.todos = response;
-      this.dataLoaded=true;
+      this.dataLoaded = true;
+      this.toastrService.success('ssdas');
     });
   }
 
-  getTodosByCategory(categoryId:number) {
+  getTodosByCategory(categoryId: number) {
     this.todoService.getTodosByCategory(categoryId).subscribe((response) => {
-     
       this.todos = response;
-      this.dataLoaded=true;
+      this.dataLoaded = true;
     });
   }
 
-  addToCart(todo:Todo){
-
+  addToCart(todo: Todo) {
+    
+    this.toastrService.success('Sepete Eklendi', todo.title);
   }
 }
